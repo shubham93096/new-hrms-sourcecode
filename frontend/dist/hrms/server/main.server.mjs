13c027a60f60247756fee9e1765ac54a7bb7c614
root <root@ip-172-31-6-147.us-east-2.compute.internal>
2026-01-28 05:19:00 +0000

    all added

---
+++ frontend/dist/hrms/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-6VE4MNZC.mjs";import"./chunk-ONHPAJ2H.mjs";import"./chunk-WXVZ73UR.mjs";import"./chunk-FME56UVT.mjs";export{a as default};
---
+++ frontend/dist/hrms/server/render-utils.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{m as o,n as r,o as n}from"./chunk-ONHPAJ2H.mjs";import{Xb as e}from"./chunk-WXVZ73UR.mjs";import"./chunk-FME56UVT.mjs";export{n as renderApplication,r as renderModule,e as \u0275Console,o as \u0275SERVER_CONTEXT};
